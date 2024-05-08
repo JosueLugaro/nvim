05d68d7bad4d6cfb2f7e54839aef5ff38247446a
JosueLugaro <josuelugaro15@gmail.com>
2024-05-08 00:36:47 -0500

    Initial commit

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,29 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) Configure lua language server for neovim
+-- require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+    -- Replace the language servers listed here 
+    -- with the ones you want to install
+    ensure_installed = {
+        'tsserver', 
+        'rust_analyzer',
+        'eslint',
+        'clangd',
+        'pyright',
+        'lua_ls'
+    },
+    handlers = {
+        function(server_name)
+            require('lspconfig')[server_name].setup({})
+        end,
+    },
+})
---
+++ lua/theakelement/init.lua
@@ -0,0 +1,3 @@
+require("theakelement.remap")
+require("theakelement.set")
+require("theakelement.plugins")
