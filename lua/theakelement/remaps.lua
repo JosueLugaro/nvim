a4ea74ba79d7a2cba3f2982caf079fe541907d28
Josue Lugaro <josuelugaro15@gmail.com>
2024-12-26 14:26:21 -0600

    Added remap for copying to system clipboard on mac

--- lua/theakelement/remaps.lua
+++ lua/theakelement/remaps.lua
@@ -1,1 +1,3 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {desc = 'Go to netrw directory listing'})
+
+vim.keymap.set("v", "<leader>y", '"+y', {desc = 'Copy to system clipboard'})
