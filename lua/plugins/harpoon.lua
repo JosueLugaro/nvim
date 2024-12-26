49fd4eb50a637c1e628b729388df50cd9dc5af76
Josue Lugaro <josuelugaro15@gmail.com>
2024-12-26 14:37:43 -0600

    Changed harpoon to utilize telescope as its UI, removed random extra whitespace

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -19,7 +19,27 @@ return {
                 "<leader>h",
                 function()
                     local harpoon = require("harpoon")
-                    harpoon.ui:toggle_quick_menu(harpoon:list())
+
+                    -- basic telescope configuration
+                    local conf = require("telescope.config").values
+                    local function toggle_telescope(harpoon_files)
+                        local file_paths = {}
+                        for _, item in ipairs(harpoon_files.items) do
+                            table.insert(file_paths, item.value)
+                        end
+
+                        require("telescope.pickers").new({}, {
+                            prompt_title = "Harpoon",
+                            finder = require("telescope.finders").new_table({
+                                results = file_paths,
+                            }),
+                            previewer = conf.file_previewer({}),
+                            sorter = conf.generic_sorter({}),
+                        }):find()
+                    end
+
+                    -- Use telescope as Harpoon UI
+                    toggle_telescope(harpoon:list())
                 end,
                 desc = "Harpoon quick menu",
             },
