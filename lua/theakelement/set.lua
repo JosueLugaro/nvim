87787482f13d3e11934fc5cd39a38198f29468b4
JosueLugaro <josuelugaro15@gmail.com>
2025-01-18 23:35:21 -0600

    Added setting to jump to last cursor position in file

--- lua/theakelement/set.lua
+++ lua/theakelement/set.lua
@@ -13,3 +13,22 @@ vim.opt.smartindent = true
 vim.opt.signcolumn = 'yes'
 
 vim.g.netrw_liststyle = 3
+
+-- Global user group to register other custom autocmds
+vim.api.nvim_create_augroup('UserGroup', {})
+
+-- When editing a file, always jump to the last known cursor position.
+-- Don't do it when the position is invalid, when inside an event handler
+-- (happens when dropping a file on gvim) and for a commit message (it's
+-- likely a different one than last time).
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = 'UserGroup',
+  callback = function(args)
+    local valid_line = vim.fn.line([['"]]) >= 1 and vim.fn.line([['"]]) < vim.fn.line('$')
+    local not_commit = vim.b[args.buf].filetype ~= 'commit'
+
+    if valid_line and not_commit then
+      vim.cmd([[normal! g`"]])
+    end
+  end,
+})
