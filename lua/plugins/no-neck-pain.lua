e1e206dc4a1548dbf12a085d816cb2e571ced4df
Josue Lugaro <josuelugaro15@gmail.com>
2025-03-22 21:44:49 -0500

    Updated setup for no-neck-pain, runs on enter, width is 150 columns

--- lua/plugins/no-neck-pain.lua
+++ lua/plugins/no-neck-pain.lua
@@ -1,6 +1,14 @@
 return {
     {
         "shortcuts/no-neck-pain.nvim",
-        version = "*"
+        version = "*",
+        config = function()
+            require("no-neck-pain").setup({
+                width = 150,
+                autocmds = {
+                    enableOnVimEnter = true
+                }
+            })
+        end
     }
 }
