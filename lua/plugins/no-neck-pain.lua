9425b91417f3427458d9f59b78d2c4d1d4f7cd23
Josue Lugaro <josuelugaro15@gmail.com>
2025-09-16 15:04:58 -0500

    Added configuration for no-neck-pain side buffers as scratch pads

--- lua/plugins/no-neck-pain.lua
+++ lua/plugins/no-neck-pain.lua
@@ -4,10 +4,19 @@ return {
         version = "*",
         config = function()
             require("no-neck-pain").setup({
-                width = 150,
+                width = 115,
                 autocmds = {
                     enableOnVimEnter = true
-                }
+                },
+                buffers = {
+                    scratchPad = {
+                        enabled = true,
+                        location = "~/Documents/",
+                    },
+                    bo = {
+                        filetype = "md",
+                    },
+                },
             })
         end
     }
