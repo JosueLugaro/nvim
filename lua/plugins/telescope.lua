36b2234096a9f9d888dc58f451f1c02cea15a113
Josue Lugaro <josuelugaro15@gmail.com>
2026-07-09 00:00:14 -0500

    feat: update telescope config to specify ripgrep flags and scope search to current working directory, update ripgrep plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,18 +14,36 @@ return {
         },
         config = function()
             local telescope = require('telescope')
+            local builtin = require('telescope.builtin')
+            
             telescope.setup({
                 pickers = {
                     find_files = {
-                        hidden = true
+                        hidden = true,
+                        cwd = vim.fn.getcwd()
+                    },
+                    live_grep = {
+                        cwd = vim.fn.getcwd()
+                    }
+                },
+                defaults = {
+                    vimgrep_arguments = {
+                        'rg',
+                        '--color=never',
+                        '--no-heading',
+                        '--with-filename',
+                        '--line-number',
+                        '--column',
+                        '--smart-case',
+                        '--hidden'
                     }
                 }
             })
             telescope.load_extension('fzf')
         end,
         keys = {
-            {'<leader>ff', function() require('telescope.builtin').find_files() end, desc = 'Telescope find files'},
-            {'<leader>fg', function() require('telescope.builtin').live_grep() end, desc = 'Telescope live grep'},
+            {'<leader>ff', function() require('telescope.builtin').find_files({ cwd = vim.fn.getcwd() }) end, desc = 'Telescope find files'},
+            {'<leader>fg', function() require('telescope.builtin').live_grep({ cwd = vim.fn.getcwd() }) end, desc = 'Telescope live grep'},
             {'<leader>fb', function() require('telescope.builtin').buffers() end, desc = 'Telescope buffers'},
             {'<leader>fh', function() require('telescope.builtin').help_tags() end, desc = 'Telescope help tags'}
         }
